76c3859b906ca72f60d231c024159040d1c829bb
Sameer Padala <sameer@padalas.net>
2021-07-14 20:32:54 -0700

    Fix examples on FF by removing import maps.
    https://github.com/mrdoob/three.js/issues/22137
    
    Some examples are broken on FF. This change makes them less broken.
    webgl_materials_standard_nodes.html works in FF again. The WebGPU
    examples don't work in FF, but they get further along.
    
    Tested:
     * Chrome Canary with `--enable-unsafe-webgpu`.
     * FF Public.

--- examples/jsm/renderers/webgpu/WebGPURenderer.js
+++ examples/jsm/renderers/webgpu/WebGPURenderer.js
@@ -14,7 +14,7 @@ import WebGPUNodes from './nodes/WebGPUNodes.js';
 
 import glslang from '../../libs/glslang.js';
 
-import { Frustum, Matrix4, Vector3, Color, LinearEncoding } from 'three';
+import { Frustum, Matrix4, Vector3, Color, LinearEncoding } from  '../../../../build/three.module.js';
 
 console.info( 'THREE.WebGPURenderer: Modified Matrix4.makePerspective() and Matrix4.makeOrtographic() to work with WebGPU, see https://github.com/mrdoob/three.js/issues/20276.' );
 
